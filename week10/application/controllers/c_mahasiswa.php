de7b826e08a90e422d5b0f2f9eb2aad1a8e2999f
MuminatulFaizah <muminatulfaizah25@gmail.com>
2019-11-21 22:27:54 +0700

    Add file praktikum 10

---
+++ week10/application/controllers/c_mahasiswa.php
@@ -0,0 +1,17 @@
+<?php
+class C_Mahasiswa extends CI_Controller{
+	public function index() {
+		$this->load->model('m_mahasiswa');
+
+		$get = $this->m_mahasiswa->get();
+		// print_r($get)
+		$data['nim'] = $get[0]->nim;
+		$data['nama'] = $get[0]->nama;
+		$data['ttl'] = $get[0]->ttl;
+		$data['alamat'] = $get[0]->alamat;
+
+		$this->load->view('v_mahasiswa', $data);
+	}
+}
+
+?>
